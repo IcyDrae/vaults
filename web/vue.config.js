59e7d7893f5c391b6ebee5b8d273521a32ff649c
MatrixEternal <35934418+MatrixEternal@users.noreply.github.com>
2021-08-10 20:30:50 +0200

    frontend: added vue router Changes to be committed: 	modified:   web/package-lock.json 	modified:   web/package.json 	modified:   web/src/App.vue 	modified:   web/src/main.js 	new file:   web/src/router/index.js 	new file:   web/src/views/About.vue 	new file:   web/src/views/Home.vue 	new file:   web/vue.config.js

---
+++ web/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    configureWebpack: {
+        devServer: {
+            host: 'password-manager.dev.local',
+            port: '1024',
+            https: true
+        }
+    }
+}
