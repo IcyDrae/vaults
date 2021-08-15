4ae78230ee522c651a638ddf378518c28c946ab2
MatrixEternal <35934418+MatrixEternal@users.noreply.github.com>
2021-08-15 02:35:44 +0200

    fix warning VeeValidate

--- web/src/main.js
+++ web/src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import VeeValidate from 'vee-validate';
 
-createApp(App).use(router, VeeValidate).mount('#app')
+createApp(App).use(router).mount('#app')
