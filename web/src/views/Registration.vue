379604101afb0c83335add4477e29d688c67f2d5
MatrixEternal <35934418+MatrixEternal@users.noreply.github.com>
2021-08-15 03:02:41 +0200

    feature: validation scheme indentation, using correct submit slot(for AJAX), resetting form Changes to be committed: 	modified:   src/App.vue 	deleted:    src/views/About.vue 	modified:   src/views/Registration.vue

--- web/src/views/Registration.vue
+++ web/src/views/Registration.vue
@@ -1,37 +1,39 @@
 <template>
   <div>
-    <VeeValidateForm @submit="submit" :validation-schema="schema" v-slot="{ errors }">
-      <label>
-        First Name
-        <VeeValidateField name="first_name" type="text" />
-      </label>
+    <VeeValidateForm :validation-schema="schema" v-slot="{ errors, handleSubmit }" as="div">
+      <form @submit="handleSubmit($event, submit)">
+          <label>
+            First Name
+            <VeeValidateField name="first_name" type="text" />
+          </label>
 
-      <label>
-        Last Name
-        <VeeValidateField name="last_name" type="text" />
-      </label>
+          <label>
+            Last Name
+            <VeeValidateField name="last_name" type="text" />
+          </label>
 
-      <label>
-        E-Mail
-        <VeeValidateField name="email" type="email" />
-      </label>
+          <label>
+            E-Mail
+            <VeeValidateField name="email" type="email" />
+          </label>
 
-      <label>
-        Password
-        <VeeValidateField name="password" type="password" />
-      </label>
+          <label>
+            Password
+            <VeeValidateField name="password" type="password" />
+          </label>
 
-      <label>
-        Password confirmation
-        <VeeValidateField name="password_confirmation" type="password" />
-      </label>
+          <label>
+            Password confirmation
+            <VeeValidateField name="password_confirmation" type="password" />
+          </label>
 
-      <br>
-      <li v-for="(message, field) in errors" :key="field">
-        {{ message }}
-      </li>
+          <br>
+          <li v-for="(message, field) in errors" :key="field">
+            {{ message }}
+          </li>
 
-      <button>Register</button>
+          <button>Register</button>
+      </form>
     </VeeValidateForm>
   </div>
 </template>
@@ -44,16 +46,34 @@ import * as yup from 'yup';
 export default {
   name: "Registration",
   components: {
+    // Rename the components from VeeValidate so there may be no conflicts with native HTML elements.
     VeeValidateForm: VeeValidate.Form,
     VeeValidateField: VeeValidate.Field,
   },
   data() {
+    // Validation schema using 'yup'.
     const schema = yup.object({
-      first_name: yup.string().required().label("First name"),
-      last_name: yup.string().required().label("Last name"),
-      email: yup.string().required().email().label("E-Mail"),
-      password: yup.string().required().min(16).label("Password"),
-      password_confirmation: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match').label("Password confirmation"),
+      first_name: yup.string()
+                  .required()
+                  .label("First name"),
+      last_name: yup.string()
+                  .required()
+                  .label("Last name"),
+      email: yup.string()
+              .required()
+              .email()
+              .label("E-Mail"),
+      password: yup.string()
+                .required()
+                .matches(
+                    /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{16,}$/,
+                    "The password must Contain at least 16 characters, one uppercase, one lowercase, one number and one special case character."
+                )
+                .label("Password"),
+      password_confirmation: yup.string()
+                              .required()
+                              .oneOf([yup.ref('password'), null], 'Passwords must match')
+                              .label("Password confirmation"),
     });
 
     return {
@@ -61,8 +81,10 @@ export default {
     };
   },
   methods: {
-    submit(values) {
+    submit(values, { resetForm }) {
       console.log(values);
+
+      resetForm();
     }
   }
 }
