94fb371311918c5718f707297ae18f0cede0c156
MatrixEternal <35934418+MatrixEternal@users.noreply.github.com>
2021-08-21 22:27:30 +0200

    added some dummy response to homepage temporary

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,12 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     public function index(): Response
     {
-        return $this->render('default/index.html.twig');
+        return $this->json([
+            "home" => true
+        ]);
     }
 }
