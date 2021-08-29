418171349be71560e1465c74e526769b70bfbf7c
MatrixEternal <35934418+MatrixEternal@users.noreply.github.com>
2021-08-30 00:00:52 +0200

    feature(spa): styling WIP

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,34 +1,55 @@
 <template>
-  <div id="nav">
+  <div class="dashboard">
+    <div class="navigation">
+      <div class="folders">
+        <p>Social Networks</p>
+        <p>Online Banking</p>
+        <p>Shopping</p>
+      </div>
+      <p>Settings</p>
+      <p>Trash</p>
+    </div>
+    <div class="logins">
+      <p>Facebook</p>
+      <p>Instagram</p>
+      <p>Medium</p>
+      <p>Reddit</p>
+      <p>Discord</p>
+      <p>Slack</p>
+      <p>Twitter</p>
+    </div>
+    <div class="login">
+      <form action="#">
+        <label>
+          Username or E-Mail
+          <input type="text">
+        </label>
+        <label>
+          Password
+          <input type="text">
+        </label>
+        <label>
+          Website
+          <input type="text">
+        </label>
+        <label>
+          Notes
+          <textarea cols="30" rows="10"></textarea>
+        </label>
+      </form>
+    </div>
+  </div>
+<!--  <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/registration">Registration</router-link> |
     <router-link to="/login">Login</router-link> |
     <router-link to="/profile">Profile</router-link> |
     <router-link to="/logout">Logout</router-link>
   </div>
-  <router-view/>
+  <router-view/>-->
 </template>
 
 <style lang="scss">
 @import "./styles/index.scss";
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
