02b37c739ba6f55a30d55f78865bbdf9ef2bc384
MatrixEternal <35934418+MatrixEternal@users.noreply.github.com>
2021-09-20 01:57:08 +0200

    frontend: implemented enc+dec of data using the enc-key

--- web/src/encryption-flow/Encryption.js
+++ web/src/encryption-flow/Encryption.js
@@ -1,4 +1,5 @@
 import * as pbkdf2 from "pbkdf2";
+import * as aes from "aes-js";
 
 export default class Encryption {
 
@@ -16,5 +17,22 @@ export default class Encryption {
                 resolve(encryptionKey);
             });
         })
+    }
+
+    encrypt(data, encryptionKey) {
+        let aesInstance = new aes.ModeOfOperation.cbc(encryptionKey);
+
+        let dataInBytes = aes.utils.utf8.toBytes(data);
+        let encryptedBytes = aesInstance.encrypt(aes.padding.pkcs7.pad(dataInBytes));
+
+        return aes.utils.hex.fromBytes(encryptedBytes);
+    }
+
+    decrypt(encryptedData, encryptionKey) {
+        let aesInstance = new aes.ModeOfOperation.cbc(encryptionKey);
+        let dataInBytes = aes.utils.hex.toBytes(encryptedData);
+        let decryptedBytes = aesInstance.decrypt(dataInBytes);
+
+        return aes.utils.utf8.fromBytes(aes.padding.pkcs7.strip(decryptedBytes));
     }
 }
