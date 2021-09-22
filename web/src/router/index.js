c3261691c483ce012253ec541a55c801c9db5e19
MatrixEternal <35934418+MatrixEternal@users.noreply.github.com>
2021-09-22 02:00:37 +0200

    frontend: added Dashboard route and view, fixed some css bugs

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,35 +1,41 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router"
+import Home from "../views/Home.vue"
 import Registration from "../views/Registration";
 import Login from "../views/Login";
-import Profile from "../views/Profile";
 import Logout from "../views/Logout";
+import Profile from "../views/Profile";
+import Dashboard from "../views/Dashboard";
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: "/",
+    name: "Home",
     component: Home
   },
   {
-    path: '/registration',
-    name: 'Registration',
+    path: "/registration",
+    name: "Registration",
     component: Registration
   },
   {
-    path: '/login',
-    name: 'login',
+    path: "/login",
+    name: "login",
     component: Login
   },
   {
-    path: '/profile',
-    name: 'profile',
+    path: "/logout",
+    name: "logout",
+    component: Logout
+  },
+  {
+    path: "/profile",
+    name: "profile",
     component: Profile
   },
   {
-    path: '/logout',
-    name: 'logout',
-    component: Logout
+    path: "/dashboard",
+    name: "dashboard",
+    component: Dashboard
   }
 ]
 
---
+++ web/src/views/Dashboard.vue
@@ -0,0 +1,189 @@
+<template>
+  <div class="dashboard">
+    <div class="navigation">
+      <ul class="folders">
+        <li>
+          <a href="#">Social Networks</a>
+        </li>
+        <li>
+          <a href="#">Online Banking</a>
+        </li>
+        <li>
+          <a href="#">Shopping</a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <a href="#">Favourites</a>
+        </li>
+        <li>
+          <a href="#">Settings</a>
+        </li>
+        <li>
+          <a href="#">Trash</a>
+        </li>
+      </ul>
+    </div>
+    <div class="logins">
+      <div class="logins-container">
+        <div class="login">
+          <div>
+            <img src="@/assets/instagram.png" alt="Instagram">
+          </div>
+          <p>Facebook</p>
+        </div>
+        <div class="login">
+          <div>
+            <img src="@/assets/instagram.png" alt="Instagram">
+          </div>
+          <p>Instagram</p>
+        </div>
+        <div class="login">
+          <div>
+            <img src="@/assets/instagram.png" alt="Instagram">
+          </div>
+          <p>Medium</p>
+        </div>
+        <div class="login">
+          <div>
+            <img src="@/assets/instagram.png" alt="Instagram">
+          </div>
+          <p>Reddit</p>
+        </div>
+        <div class="login">
+          <div>
+            <img src="@/assets/instagram.png" alt="Instagram">
+          </div>
+          <p>Discord</p>
+        </div>
+        <div class="login">
+          <div>
+            <img src="@/assets/instagram.png" alt="Instagram">
+          </div>
+          <p>Slack</p>
+        </div>
+        <div class="login">
+          <div>
+            <img src="@/assets/instagram.png" alt="Instagram">
+          </div>
+          <p>Twitter</p>
+        </div>
+        <div class="login">
+          <div>
+            <img src="@/assets/instagram.png" alt="Instagram">
+          </div>
+          <p>Reddit</p>
+        </div>
+        <div class="login">
+          <div>
+            <img src="@/assets/instagram.png" alt="Instagram">
+          </div>
+          <p>Discord</p>
+        </div>
+        <div class="login">
+          <div>
+            <img src="@/assets/instagram.png" alt="Instagram">
+          </div>
+          <p>Slack</p>
+        </div>
+        <div class="login">
+          <div>
+            <img src="@/assets/instagram.png" alt="Instagram">
+          </div>
+          <p>Twitter</p>
+        </div>
+        <div class="login">
+          <div>
+            <img src="@/assets/instagram.png" alt="Instagram">
+          </div>
+          <p>Reddit</p>
+        </div>
+        <div class="login">
+          <div>
+            <img src="@/assets/instagram.png" alt="Instagram">
+          </div>
+          <p>Discord</p>
+        </div>
+        <div class="login">
+          <div>
+            <img src="@/assets/instagram.png" alt="Instagram">
+          </div>
+          <p>Slack</p>
+        </div>
+        <div class="login">
+          <div>
+            <img src="@/assets/instagram.png" alt="Instagram">
+          </div>
+          <p>Twitter</p>
+        </div>
+
+      </div>
+    </div>
+    <div class="account">
+      <form action="#">
+        <label>
+          <span>Username or E-Mail</span>
+          <input type="text">
+        </label>
+        <label>
+          <span>Password</span>
+          <input type="text">
+        </label>
+        <label>
+          <span>Password</span>
+          <input type="text">
+        </label>
+        <label>
+          <span>Password</span>
+          <input type="text">
+        </label>
+        <label>
+          <span>Password</span>
+          <input type="text">
+        </label>
+        <label>
+          <span>Password</span>
+          <input type="text">
+        </label>
+        <label>
+          <span>Password</span>
+          <input type="text">
+        </label>
+        <label>
+          <span>Password</span>
+          <input type="text">
+        </label>
+        <label>
+          <span>Website</span>
+          <input type="text">
+        </label>
+        <label>
+          <span>Notes</span>
+          <textarea cols="30" rows="10"></textarea>
+        </label>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+  data() {
+    return {
+    }
+  },
+  beforeRouteEnter(to, from, next) {
+    document.querySelector("#app").classList.add("dashboard-view")
+    next();
+  },
+  beforeRouteLeave(to, from, next) {
+    document.querySelector("#app").classList.remove("dashboard-view")
+    next();
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
