4ddef6d4d5b89c4d14717f4c89a234a2ac5b6d5c
MatrixEternal <35934418+MatrixEternal@users.noreply.github.com>
2021-09-23 21:02:45 +0200

    api: VaultController list method

--- src/Controller/VaultController.php
+++ src/Controller/VaultController.php
@@ -2,14 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Vault;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class VaultController extends AbstractController
 {
-    public function list(): Response
+    /**
+     * Queries the repository by the given user id and returns all(if any) found vaults.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function list(Request $request): Response
     {
-        return new Response();
+        $vaults = $this->getDoctrine()
+                        ->getRepository(Vault::class)
+                        ->findByUserId($request->get("userId"));
+
+        return new JsonResponse([
+            "vaults" => $vaults
+        ]);
     }
 
     public function create(): Response
