3175f2adfb75c89e4b2c0accf1fa50db49825be8
MatrixEternal <35934418+MatrixEternal@users.noreply.github.com>
2021-09-24 22:44:06 +0200

    frontend: decrypt fetched vaults

--- web/src/views/Vaults/Vaults.vue
+++ web/src/views/Vaults/Vaults.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="vaults">
     <div v-for="vault in vaults" :key="vault" class="vault">
-      <p>{{ vault.name }}</p>
+      <p>{{ vault.vault_name }}</p>
       <p>37 Items</p>
-      <p class="vault-description">{{ vault.description }}</p>
+      <p class="vault-description">{{ vault.vault_name }}</p>
     </div>
     <div class="create-vault-cta">
       <router-link to="/vaults/create">
@@ -17,6 +17,7 @@
 <script>
 
 import axios from "axios";
+import Encryption from "../../encryption-flow/Encryption";
 import { createNamespacedHelpers } from 'vuex';
 
 const { mapGetters } = createNamespacedHelpers("user");
@@ -25,8 +26,9 @@ export default {
   name: "Vaults",
   data() {
     return {
-      vaults: JSON,
+      vaults: [],
       backendErrors: [],
+      encryption: new Encryption()
     }
   },
   mounted() {
@@ -34,10 +36,14 @@ export default {
   },
   computed: {
     ...mapGetters([
-      "getUser",
+        "getUser",
+        "getEncryptionKey"
     ])
   },
   methods: {
+    /**
+     * Requests the user's encrypted vaults.
+     */
     fetchVaults() {
       axios
           .get(process.env.VUE_APP_API_HOSTNAME + "/vaults", {
@@ -52,14 +58,31 @@ export default {
           })
           .then(response => {
             if(response.status === 200) {
-              this.vaults = response.data.vaults;
+              this.vaults = this.decryptVaults(response.data.vaults);
             }
           })
           .catch(error => {
-            if (error.response.data.registration === false) {
-              this.backendErrors.push(error.response.data.errors)
-            }
+            this.backendErrors.push(error.response.data.errors)
           })
+    },
+    /**
+     * Decrypts the given vaults & decodes them into an array.
+     *
+     * @param vaults
+     * @returns {*[]}
+     */
+    decryptVaults(vaults) {
+      let decryptedVaults = [];
+
+      vaults.forEach((vault, index) => {
+        vaults[index] = this.encryption.decrypt(vault.data, this.getEncryptionKey);
+
+        let jsonDecodedVault = JSON.parse(vaults[index]);
+
+        decryptedVaults.push(jsonDecodedVault);
+      });
+
+      return decryptedVaults;
     }
   }
 }
