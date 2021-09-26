35fc08be761af9b62a9edd74ee3d04cb672a9254
MatrixEternal <35934418+MatrixEternal@users.noreply.github.com>
2021-09-26 20:16:59 +0200

    feature(frontend): delete vault functionality & styles

--- web/src/views/Vaults/DeletePrompt.vue
+++ web/src/views/Vaults/DeletePrompt.vue
@@ -1,11 +1,24 @@
 <template>
-$END$
+  <div class="delete-vault-overlay vault-overlay">
+    <div class="prompt">
+      <div class="prompt-message">
+        You are about to delete this vault & all items inside it. This cannot be undone. Are you absolutely sure?
+      </div>
+
+      <div>
+        <button class="btn delete-vault-cta" type="button" @click="$emit('deletionConfirmed')">Delete</button>
+        <button class="btn" type="button" @click="$emit('deletionCancelled')">No, take me back</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+
 export default {
-name: "DeletePrompt"
+  name: "DeletePrompt"
 }
+
 </script>
 
 <style scoped>
--- web/src/views/Vaults/EditVault.vue
+++ web/src/views/Vaults/EditVault.vue
@@ -20,7 +20,8 @@
           <p class="form-error">{{ errors.vault_description }}</p>
         </label>
 
-        <button>Save</button>
+        <button class="btn">Save changes</button>
+        <button type="button" class="btn delete-vault-cta" @click="showModal = true">Delete vault</button>
 
         <li class="backend-errors" v-for="error in backendErrors" :key="error">
           {{ error }}
@@ -29,11 +30,18 @@
         <p class="backend-success">{{ success }}</p>
       </form>
     </VeeValidateForm>
+    <transition name="modal">
+      <DeletePrompt v-if="showModal"
+                    @deletionConfirmed="deleteVault"
+                    @deletionCancelled="showModal = false">
+      </DeletePrompt>
+    </transition>
   </div>
 </template>
 
 <script>
 
+import DeletePrompt from "./DeletePrompt";
 import * as VeeValidate from "vee-validate";
 import * as yup from "yup";
 import Encryption from "../../encryption-flow/Encryption";
@@ -48,13 +56,15 @@ export default {
     // Rename the components from VeeValidate so there may be no conflicts with native HTML elements.
     VeeValidateForm: VeeValidate.Form,
     VeeValidateField: VeeValidate.Field,
+    DeletePrompt
   },
   props: ["vault_name", "vault_description"],
   data() {
     return {
       success: "",
       backendErrors: [],
-      encryption: new Encryption()
+      encryption: new Encryption(),
+      showModal: false
     }
   },
   computed: {
@@ -107,6 +117,28 @@ export default {
           .then(response => {
             if(response.status === 204) {
               resetForm();
+
+              this.$router.go(-1);
+            }
+          })
+          .catch(error => {
+            this.backendErrors.push(error.response.data.errors)
+          });
+    },
+    deleteVault() {
+      axios
+          .delete(process.env.VUE_APP_API_HOSTNAME + `/vaults/delete/${this.$route.params.id}`, {
+            headers: {
+              "Content-Type": "application/json",
+            },
+            withCredentials: true,
+            data: JSON.stringify({
+              userId: this.getUser.id,
+            })
+          })
+          .then(response => {
+            if(response.status === 204) {
+              this.showModal = false;
 
               this.$router.go(-1);
             }
