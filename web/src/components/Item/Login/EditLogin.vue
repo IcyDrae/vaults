afe3669ac9a6114d6dfb1b615ba204f530833d28
MatrixEternal <35934418+MatrixEternal@users.noreply.github.com>
2021-10-06 22:56:05 +0200

    frontend: refactoring forms to have common form models for each type of entity

--- web/src/components/Item/Login/EditLogin.vue
+++ web/src/components/Item/Login/EditLogin.vue
@@ -11,13 +11,12 @@
 
 <script>
 
-import {api} from "../../../services/api";
 import Form from "../../../components/Item/Login/Form";
 import Encryption from "../../../encryption-flow/Encryption";
 import { createNamespacedHelpers } from 'vuex';
 import http from "../../../services/http";
 
-const { mapGetters } = createNamespacedHelpers("user");
+const { mapActions, mapGetters } = createNamespacedHelpers("user");
 
 export default {
   name: "EditLogin",
@@ -33,12 +32,6 @@ export default {
       showModal: false
     }
   },
-  mounted() {
-    //let itemType = JSON.parse(this.$props.itemData).item_type;
-
-    console.log(api["note"].schema)
-
-  },
   computed: {
     ...mapGetters([
       "getUser",
@@ -52,6 +45,9 @@ export default {
     }
   },
   methods: {
+    ...mapActions([
+      "addItem"
+    ]),
     deleteHandler() {
       console.log("deleting from parent");
     },
@@ -61,14 +57,14 @@ export default {
      * @param resetForm
      */
     handleForm(values, { resetForm }) {
-      console.log(values, resetForm)
-      /*values = JSON.stringify(values);
+      values.item_type = "login";
+      values = JSON.stringify(values);
       let encryptedValues = this.encryption.encrypt(values, this.getEncryptionKey);
 
-      this.submitForm(encryptedValues, resetForm);*/
+      this.submitForm(encryptedValues, resetForm);
     },
     submitForm(values, resetForm) {
-      let url = `/vaults/update/${this.$route.params.id}`;
+      let url = `/logins/update/${this.loginData.id}`;
 
       http.request({
         method: "put",
@@ -78,14 +74,86 @@ export default {
           data: values
         }
       }).then(response => {
-        if(response.status === 204) {
-          resetForm();
-
-          this.$router.go(-1);
-        }
+        this.successHandler(response, resetForm);
       }).catch(error => {
-        this.backendErrors.push(error.response.data.errors);
+        console.log(error)
+        //this.backendErrors.push(error.response.data.errors);
       });
+    },
+    successHandler(response, resetForm) {
+      if(response.status === 200) {
+        let updatedLogin = this.decryptLogin(response.data);
+        /*this.addItem(updatedLogin);*/
+
+        console.log(updatedLogin)
+
+        console.log(resetForm)
+
+        //resetForm();
+
+        /*this.$router.push({ name: "item", params: { itemId: updatedLogin.id, itemData: JSON.stringify(updatedLogin) } });*/
+      }
+    },
+    /**
+     * Decrypts the given logins & decodes them into an array.
+     *
+     * @param response
+     * @returns {{}}
+     */
+    decryptLogin(response) {
+      let decryptedLogins = {};
+
+      let login = response.data;
+
+      login = this.encryption.decrypt(login, this.getEncryptionKey);
+      login = JSON.parse(login);
+
+      response.data = login;
+
+      decryptedLogins = this.restructureLoginObject(response);
+
+      return decryptedLogins;
+    },
+    /**
+     * Makes a new login array with the decrypted data.
+     *
+     * @param object
+     */
+    restructureLoginObject(object) {
+      return {
+        "id": object.id,
+        "name": {
+          "label": "Name",
+          "value": object.data.login_name,
+          "type": "text"
+        },
+        "login_username": {
+          "label": "Username",
+          "value": object.data.login_username,
+          "type": "text"
+        },
+        "login_email": {
+          "label": "E-Mail",
+          "value": object.data.login_email ?? "",
+          "type": "text"
+        },
+        "login_website": {
+          "label": "Website",
+          "value": object.data.login_website,
+          "type": "text"
+        },
+        "login_password": {
+          "label": "Password",
+          "value": object.data.login_password,
+          "type": "password"
+        },
+        "login_description": {
+          "label": "Description",
+          "value": object.data.login_description ?? "",
+          "type": "textarea"
+        },
+        "item_type": object.data.item_type
+      };
     },
     deleteVault() {
       let url = `/vaults/delete/${this.$route.params.id}`;
